061ad4d6db65d813870abd95d609cd575214a563
Markus Wamser <wamser@tum.de>
2016-09-16 11:44:00 +0200

    Initial main code file

---
+++ R/WMisc.R
@@ -0,0 +1,16 @@
+# ====================
+# Main WMisc
+# ====================
+
+#' WMisc -- Misc. helper functions and tools.
+#'
+#' This package contains a set of helper functions and tools that I couldn't find elsewhere or where I needed some slightly different behaviour.
+#'
+#' @section File access
+#'
+#'
+#' @docType package
+#' @name WMisc
+NULL
+#>NULL
+
